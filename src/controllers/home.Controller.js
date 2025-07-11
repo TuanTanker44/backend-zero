708e6195af3b9651fa6cec930daad6300ab1234e
Tuấn Ngô <tuan9864543@gmail.com>
2025-07-11 11:48:36 +0700

    tao module database, test query

--- src/controllers/home.Controller.js
+++ src/controllers/home.Controller.js
@@ -1,5 +1,21 @@
+const connection = require('../config/database');
+let users = [];
+
+
 const getHomePage = (req, res) => {
-    res.send('Hello World!');
+    connection.query(
+        'select * from Users', 
+        function (error, results, fields) {
+            if (error) {
+            console.error('Error in query:', error.message);
+            return;
+            }
+            users = results; 
+            console.log('>>>Results: ', results); // Log the results of the query
+
+            console.log('>>>Users: ', users); // Log the users array
+            res.send(JSON.stringify(users));
+    });
 }
 
 const getSamplePage = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require('express') //commonjs
 const app = express() // Import express module
 const configViewEngine = require('./config/viewEngine') // Import the view engine configuration
 const webRoutes = require('./routes/web') // Import the web routes
-const mysql = require('mysql2') // Import MySQL2 for database connection
+const connection = require('./config/database') // Import the database connection
 
 
 const port = process.env.PORT || 8888 // Set the port from environment variable
@@ -16,24 +16,6 @@ configViewEngine(app)
 //khai báo route
 app.use('/', webRoutes) // Use the web routes for the root path
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
-
-connection.query(
-  'select * from Users', 
-  function (error, results, fields) {
-    if (error) {
-      console.error('Error in query:', error.message);
-      return;
-    }
-    console.log('Results: ', results); // Log the results of the query
-    console.log('Fields: ', fields); // Log the fields of the query
-  });
 
 app.listen(port, localhost, () => {
   console.log(`Example app listening at http://${localhost}:${port}`)
