b3f99449ea678f0884d5e87e5543d853055c89e5
Tuấn Ngô <tuan9864543@gmail.com>
2025-07-11 12:03:22 +0700

    su dung createPool thay vi createConnection khi ket noi database

--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,23 @@ require('dotenv').config()
 const mysql = require('mysql2') // Import MySQL2 for database connection
 
 //create a database connection
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// });
+
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    waitForConnections: true, // cho phép chờ đợi khi tất cả kết nối đang bận
+    connectionLimit: 10, // giới hạn số lượng kết nối đồng thời
+    queueLimit: 0 // không giới hạn hàng đợi kết nối, >0 : giới hạn số lượng hàng đợi khi tất cả kết nối đang bận
 });
   
 module.exports = connection;
